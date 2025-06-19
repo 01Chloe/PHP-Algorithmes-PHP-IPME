44b374ca50f3c5197dbb252e8b385b769f5c1bdb
01Chloe <allier.chloe@gmaim.com>
2025-06-19 10:44:57 +0200

    exo1

---
+++ exos_associatif/exo1.php
@@ -0,0 +1,12 @@
+<?php
+$personne = [
+    'Nom' => 'Dupont',
+    'Prenom' => 'Alice',
+    'Age' => 25
+];
+
+echo $personne['Prenom'] . " " . $personne['Nom'] . " a " . $personne['Age'] . ' ans.';
+
+
+
+?>
