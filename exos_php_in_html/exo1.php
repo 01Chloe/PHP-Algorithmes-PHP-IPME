0b526033c5e25d7f469804f1b959ae1395619b42
01Chloe <allier.chloe@gmaim.com>
2025-06-19 11:22:06 +0200

    exo1

---
+++ exos_php_in_html/exo1.php
@@ -0,0 +1,19 @@
+<?php
+$titre = "Bienvenue sur mon site";
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo1</title>
+</head>
+<body>
+    <div>
+        <h1><?= $titre ?></h1>
+    </div>    
+
+</body>
+</html>
